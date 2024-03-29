1d0e5dbb13db57e0c725363b92c76fa19fadb335
ettie <ettienemare@gmail.com>
2024-03-29 14:27:38 +0200

    refactor code

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,33 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
-using System.Text.Unicode;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Extensions.ManagedClient;
 using MQTTnet.Formatter;
 using MQTTnet.Protocol;
 
-Console.WriteLine("Hello, World!");
-
-var p = new Person { Name = "Ettiene", Age = 40 };
-p.Parent = p;
-
-var json = System.Text.Json.JsonSerializer.Serialize(p);
-
-Console.WriteLine(json);
-
-return;
-
-class Person
-{
-    public required string Name { get; set; }
-    public int Age { get; set; }
-    
-    public Person? Parent { get; set; }
-}
-
-/*
 var mqttClient = new MqttClient();
 await mqttClient.ConnectAsync();
 
@@ -37,8 +16,12 @@ var result = "";
 
 try
 {
-    var action = new MqttAction("elm/test", $"Ettiene {Random.Shared.Next(10, 99)}", "elm/response");
-    result = await MqttActionWrapper.ExecuteAsync(action, mqttClient, cancellationTokenSource.Token);
+    for (int i = 0; i < 20; i++)
+    {
+        var action = new MqttAction("elm/test", $"Ettiene {Random.Shared.Next(10, 99)}", "elm/response");
+        result = await MqttActionWrapper.ExecuteAsync(action, mqttClient, cancellationTokenSource.Token);
+        Console.WriteLine(result);
+    }
 }
 catch (Exception e)
 {
@@ -48,8 +31,6 @@ finally
 {
     await mqttClient.DisconnectAsync();
 }
-
-Console.WriteLine(result);
 Console.WriteLine("Done");
 
 public record struct MqttCommand(string Topic, string? Payload, Guid? CorrelationId = default);
@@ -58,29 +39,25 @@ public record struct MqttAction(string Topic, string Payload, string ResponseTop
 
 public class MqttActionWrapper
 {
-    private readonly MqttClient _client;
-    public MqttAction Action { get; set; }
-    public TaskCompletionSource<string> TaskCompletionSource { get; set; }
-    public Guid CorrelationId { get; set; }
-    private TaskCompletionSource<string> _tcs = new ();
+    private readonly Guid _correlationId;
+    private readonly TaskCompletionSource<string> _tcs = new ();
 
-    private MqttActionWrapper(MqttAction action, MqttClient client)
+    private MqttActionWrapper(MqttClient client)
     {
-        _client = client;
-        Action = action;
-        TaskCompletionSource = new TaskCompletionSource<string>();
-        CorrelationId = Guid.NewGuid();
+        _correlationId = Guid.NewGuid();
         client.ApplicationMessageReceivedAsync += OnApplicationMessageReceivedAsync;
     }
 
     public static async Task<string> ExecuteAsync(MqttAction action, MqttClient client, CancellationToken token)
     {
         var a = action with { ResponseTopic = $"{action.ResponseTopic}@{Guid.NewGuid()}" };
-        var wrapper = new MqttActionWrapper(a, client);
+        var wrapper = new MqttActionWrapper(client);
         await client.SubscribeAsync(a.ResponseTopic);
-        await client.EnqueueAsync(new MqttCommand(action.Topic, action.Payload, wrapper.CorrelationId), a.ResponseTopic);
+        await client.SubscribeAsync($"{Settings.ErrorTopic}/{wrapper._correlationId}");
+        await client.EnqueueAsync(new MqttCommand(action.Topic, action.Payload, wrapper._correlationId), a.ResponseTopic);
         var rsp = await wrapper.WaitAsync(token);
         await client.UnsubscribeAsync(a.ResponseTopic);
+        await client.UnsubscribeAsync($"{Settings.ErrorTopic}/{wrapper._correlationId}");
         await client.EnqueueAsync(new MqttCommand(a.ResponseTopic, null));
         
         return rsp;
@@ -94,7 +71,7 @@ public class MqttActionWrapper
 
     private Task OnApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
     {
-        if(arg.ApplicationMessage.Topic == Action.ResponseTopic && arg.ApplicationMessage.CorrelationData.SequenceEqual(CorrelationId.ToByteArray()))
+        if(arg.ApplicationMessage.CorrelationData.SequenceEqual(_correlationId.ToByteArray()))
         {
             _tcs.TrySetResult(Encoding.UTF8.GetString(arg.ApplicationMessage.PayloadSegment));
         }
@@ -103,41 +80,56 @@ public class MqttActionWrapper
     }
 }
 
-
 public class MqttClient
 {
     private readonly IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
     
     public event Func<MqttApplicationMessageReceivedEventArgs, Task>? ApplicationMessageReceivedAsync;
 
-    public MqttClient()
-    {
+    public MqttClient() => 
         _mqttClient.ApplicationMessageReceivedAsync += MqttClientOnApplicationMessageReceivedAsync;
-    }
 
     private async Task MqttClientOnApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
     {
             if (arg.ApplicationMessage.ResponseTopic != null)
             {
-                var b = string.Empty;
+                var payload = string.Empty;
                 
                 if (arg.ApplicationMessage.PayloadSegment.Array != null)
-                    b = Encoding.UTF8.GetString(arg.ApplicationMessage.PayloadSegment.Array);
+                    payload = Encoding.UTF8.GetString(arg.ApplicationMessage.PayloadSegment.Array);
+                
+                var msg = Random.Shared.Next(0, 99) % 5 == 0 ? GetErrorMessage(payload)  : GetResponseMessage(payload);
 
-                var response = new MqttApplicationMessageBuilder()
+                await _mqttClient.EnqueueAsync(msg);
+                return;
+            }
+
+            if (ApplicationMessageReceivedAsync != null) await ApplicationMessageReceivedAsync(arg);
+            return;
+
+            MqttApplicationMessage GetResponseMessage(string payload)
+            {
+                return new MqttApplicationMessageBuilder()
                     .WithTopic(arg.ApplicationMessage.ResponseTopic)
-                    .WithPayload($"Hello {Random.Shared.Next(10, 99)} {b}")
+                    .WithPayload($"Hello {Random.Shared.Next(10, 99)} {payload}")
                     .WithCorrelationData(arg.ApplicationMessage.CorrelationData)
                     .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
                     .WithRetainFlag()
                     .WithMessageExpiryInterval(60)
                     .Build();
-                
-                await _mqttClient.EnqueueAsync(response);
-                return;
             }
-
-            if (ApplicationMessageReceivedAsync != null) await ApplicationMessageReceivedAsync(arg);
+            
+            MqttApplicationMessage GetErrorMessage(string payload)
+            {
+                return new MqttApplicationMessageBuilder()
+                    .WithTopic($"{Settings.ErrorTopic}/{new Guid(arg.ApplicationMessage.CorrelationData)}")
+                    .WithPayload($"Error: Hello {Random.Shared.Next(10, 99)} {payload}")
+                    .WithCorrelationData(arg.ApplicationMessage.CorrelationData)
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                    .WithRetainFlag()
+                    .WithMessageExpiryInterval(60)
+                    .Build();
+            }
     }
 
     public async Task ConnectAsync()
@@ -146,9 +138,8 @@ public class MqttClient
             .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
             .WithClientOptions(new MqttClientOptionsBuilder()
                 .WithClientId("Client1")
-                .WithTcpServer("dev.caracal.com", 1883)
+                .WithTcpServer(Settings.MqttServer, Settings.MqttPort)
                 .WithProtocolVersion(MqttProtocolVersion.V500)
-                //.WithCredentials("bud", "%spencer%")
                 .WithCleanSession()
                 .Build())
             .Build();
@@ -167,18 +158,23 @@ public class MqttClient
             .WithTopic(command.Topic)
             .WithPayload(command.Payload)
             .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
-            .WithMessageExpiryInterval(60)
+            .WithMessageExpiryInterval(Settings.Timeout)
             .WithRetainFlag();
 
         if (responseTopic is not null)
             message = message.WithResponseTopic(responseTopic);
 
-        var message2 = message.WithCorrelationData((command.CorrelationId ?? Guid.NewGuid()).ToByteArray())
-            .Build();
+        var message2 = message.WithCorrelationData((command.CorrelationId ?? Guid.NewGuid()).ToByteArray()).Build();
 
         await SubscribeAsync(command.Topic).ConfigureAwait(false);
-
         await _mqttClient.EnqueueAsync(message2).ConfigureAwait(false);
     }
 }
-*/
+
+public static class Settings
+{
+    public static string MqttServer => "dev.caracal.com";
+    public static int MqttPort => 1883;
+    public static uint Timeout => 60;
+    public static string ErrorTopic => "errors/action";
+}
