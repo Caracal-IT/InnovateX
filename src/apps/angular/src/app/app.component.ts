7833d8b1021147e0ecdbb35047f368898a7deaf1
ettie <ettienemare@gmail.com>
2024-06-01 22:37:29 +0200

    Add Weather

--- src/apps/angular/src/app/app.component.ts
+++ src/apps/angular/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { WeatherForecasts } from '../types/weatherForecast';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'innovateX';
+  forecasts: WeatherForecasts = [];
+
+  constructor(private http: HttpClient) {
+    http.get<WeatherForecasts>('api/weatherforecast').subscribe({
+      next: result => this.forecasts = result,
+      error: console.error
+    });
+  }
 }
