f0ec1d7a66ad413aede2201afc201b29243abc9f
ettie <ettienemare@gmail.com>
2024-06-01 19:23:27 +0200

    Add vue

--- src/app-host/InnovateX.AppHost/Program.cs
+++ src/app-host/InnovateX.AppHost/Program.cs
@@ -2,11 +2,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.InnovateX_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.InnovateX_ApiService>("apiservice").WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.InnovateX_Web_Blazor>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
+
+builder.AddNpmApp("vue", "../../apps/vue")
+    .WithReference(apiService)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
